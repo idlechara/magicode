f860b44888abf52fabcf7d0cb572c179727a06f5
Erik Regla <erik.regla@gmail.com>
2020-04-22 02:44:55 -0400

    lic(added): to benchmark code

---
+++ program/base_benchmark/README.md
@@ -0,0 +1,2 @@
+# Base benchmark for IQS and IIQS for Magicode
+Feel free to use it as you want.
--- program/base_benchmark/flags.h
+++ program/base_benchmark/flags.h
@@ -1,6 +1,19 @@
-//
-// Created by kuky_nekoi on 22-04-20.
-//
+// Copyright (C) 2020 Erik Regla
+// 
+// This file is part of Magicode - (I)IQS benchmark.
+// 
+// Magicode - (I)IQS benchmark is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Magicode - (I)IQS benchmark is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Magicode - (I)IQS benchmark.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef IQS_TEST_FLAGS_H
 #define IQS_TEST_FLAGS_H
