1d6f9cd632096126a582835fbaf680583d99c7fb
Erik Regla <erik.regla@gmail.com>
2020-04-24 02:46:26 -0400

    fix(main): forgot to add it

--- program/base_benchmark/main.cpp
+++ program/base_benchmark/main.cpp
@@ -67,7 +67,7 @@ void experiment_1()
 
 
     for(size_t i=0; i<test_size; i++) {
-        TYPE_TO_USE val = rand() % (test_size + 1);
+        TYPE_TO_USE val = rand() % (test_size/2);
         a[i] = val;
         b[i] = val;
         c[i] = val;
@@ -513,6 +513,83 @@ void experiment_6()
 
 }
 
+void experiment_7()
+{
+    const size_t test_size = TEST_SIZE;
+
+    TYPE_TO_USE *a = new TYPE_TO_USE[test_size];
+    TYPE_TO_USE *g = new TYPE_TO_USE[test_size];
+
+    std::vector<TYPE_TO_USE> b;
+    std::vector<TYPE_TO_USE> c;
+    std::vector<TYPE_TO_USE> d;
+    std::vector<TYPE_TO_USE> e;
+    std::vector<TYPE_TO_USE> f;
+    std::vector<TYPE_TO_USE> result_temp;
+
+    for(size_t i=0; i<test_size; i++) {
+        TYPE_TO_USE val = 0;
+        a[i] = val;
+        b.push_back(val);
+        c.push_back(val);
+        d.push_back(val);
+        e.push_back(val);
+        f.push_back(val);
+        result_temp.push_back(val);
+        g[i] = val;
+    }
+
+
+
+    TRACK_TIME_START
+        BareBoneIQS<TYPE_TO_USE> biqs(a, test_size);
+        for(int i = 0; i < ELEMENTS_TO_EXTRACT; i++)biqs.next();
+    TRACK_TIME_END("Bare-bones C IQS")
+
+    TRACK_TIME_START
+        IQS<std::vector<TYPE_TO_USE>, TYPE_TO_USE> iqs(b);
+        for(int i = 0; i < ELEMENTS_TO_EXTRACT; i++)iqs.next();
+    TRACK_TIME_END("C++ portable IQS")
+
+    TRACK_TIME_START
+        std::partial_sort(c.begin(), c.begin() + ELEMENTS_TO_EXTRACT, c.end());
+    TRACK_TIME_END("std::partial_sort complete")
+
+    TRACK_TIME_START
+        for(int i = 0; i < ELEMENTS_TO_EXTRACT; i++) std::partial_sort(d.begin() + i, d.begin() + i, d.end());
+    TRACK_TIME_END("std::partial_sort incremental")
+
+    TRACK_TIME_START
+        std::make_heap(e.begin(), e.end());
+        for(int i = 0; i < ELEMENTS_TO_EXTRACT - 1; i++) {
+            TYPE_TO_USE temp = e.front();
+            std::pop_heap(e.begin(), e.end());
+            e.pop_back();
+        }
+    TRACK_TIME_END("std::heap incremental")
+
+    TRACK_TIME_START
+        IIQS<std::vector<TYPE_TO_USE>, TYPE_TO_USE> iiqs(f);
+        for(int i = 0; i < ELEMENTS_TO_EXTRACT; i++)iiqs.next();
+    TRACK_TIME_END("C++ portable IIQS")
+
+
+    TRACK_TIME_START
+        BareBoneIIQS<TYPE_TO_USE> biiqs(g, test_size);
+        for(int i = 0; i < ELEMENTS_TO_EXTRACT; i++)biiqs.next();
+    TRACK_TIME_END("Bare-bones C IIQS");
+
+
+    std::sort(result_temp.begin(), result_temp.end());
+    CHECK_AGAINST_BASELINE(result_temp, a);
+    CHECK_AGAINST_BASELINE(result_temp, b);
+    CHECK_AGAINST_BASELINE(result_temp, c);
+    CHECK_AGAINST_BASELINE(result_temp, d);
+    CHECK_AGAINST_BASELINE(result_temp, f);
+    CHECK_AGAINST_BASELINE(result_temp, g);
+
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -535,5 +612,8 @@ int main(int argc, char const *argv[])
 
     printf("\n\n----EXPERIMENT 5^-1 ----\n");
     experiment_6();
+
+    printf("\n\n----EXPERIMENT Only zeroes ----\n");
+    experiment_7();
 
 }
